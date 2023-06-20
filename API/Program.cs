f9f08693a35f2f2815283513722d025286db56b1
Nguyen Quang Thao <nquangthao19072004@gmail.com>
2023-06-20 16:51:22 +0700

    init client: AngularJS

--- API/Program.cs
+++ API/Program.cs
@@ -2,7 +2,6 @@ using API.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -13,9 +12,11 @@ builder.Services.AddDbContext<DataContext>(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
+app.UseCors(option => option.AllowAnyHeader().AllowAnyHeader().WithOrigins("https://localhost:4200"));
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
